217f290073598b0928afd0ab01eeb84f90937d64
Handerson <handerson.contreras@gmail.com>
2021-10-28 14:28:05 -0600

    removed unused files

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    client: {
-        service: {
-            name: 'Veritext',
-            url: "http://localhost:4000",
-            skipSSLValidation: true,
-        }
-    }
-}
